54d4f75e6096eae5cfda26455d0c8f8ec4247fb2
Awilliams4321 <awilliams1234@live.com>
2020-07-30 12:15:02 -0600

    Defining an index action method that stores all Review objects.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,14 @@
 class ReviewsController < ApplicationController
 
+    def index
+        @reviews = Review.all
+    end
+
     def new
         @review = Review.new
     end
+
+    def create
+       # @review = Review.new(review_params)
+    end 
 end
