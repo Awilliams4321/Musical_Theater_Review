317b1ca5030194600973081c33d166e5912bf377
Awilliams4321 <awilliams1234@live.com>
2020-07-30 09:58:29 -0600

    Adding a get route that maps to my 'new' review controller action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :reviews
+  get '/reviews/new', to: 'reviews#new'
+  post '/reviews', to: 'reviews#create'
   resources :musicals
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
