fe3c6b2203c42aed847755ebd29c39129ad3c5ca
Awilliams4321 <awilliams1234@live.com>
2020-07-31 11:01:35 -0600

    Defining new, create, and logout methods. The create method finds a user by their email and if the user exists and their password has been authenticated(bcrypt/has_secure_password), they will be assigned a session key and be redirected to the Musicals index page. Otherwise they will be redirected to the login page.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,33 @@
 class SessionsController < ApplicationController
 
+    def new
+    end
+
+    def create
+        user = User.find_by(email: session_params[:email])
+        
+        if user && user.authenticate(session_params[:password])
+            session[:user_id] = user.id
+            redirect_to musicals_path
+        else
+            redirect_to login_path
+        end
+    end
+
+    def logout
+        session.clear
+        redirect_to login_path
+    end
+
+    def github
+
+    end
+
+    private
+    
+    def session_params
+        params.require(:session).permit(:username, :email, :password)
+    end
+
+
 end
