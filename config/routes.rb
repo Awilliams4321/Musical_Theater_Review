5330146773cbde032014914775756fbb905ffbf0
Awilliams4321 <awilliams1234@live.com>
2020-07-30 20:13:10 -0600

    Using resources to create the only routes necessary for the User controller: new, show, and create.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get '/reviews', to: 'reviews#index'
   get '/reviews/new', to: 'reviews#new'
   post '/reviews', to: 'reviews#create'
+  get 'reviews/:id', to: 'reviews#show'
+  get '/reviews/:id/edit', to: 'reviews#edit'
   resources :musicals
-  resources :users
+  resources :users, only: [:new, :show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
