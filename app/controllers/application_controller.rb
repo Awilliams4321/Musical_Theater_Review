37c8eeba1fa0fd7c9a0e36994973c3f035c19a99
Awilliams4321 <awilliams1234@live.com>
2020-07-30 20:09:14 -0600

    Creating helper methods 'is_logged_in?' and 'current_user' to create a session and assign a user an id if they are logged in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+    helpers do 
+        def is_logged_in?
+          session.has_key?(:user_id)
+        end 
+    
+        def current_user
+          @current_user ||= User.find(session[:user_id]) if is_logged_in?
+        end
+      end 
+      
 end
