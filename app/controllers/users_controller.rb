480a061794754ad8b0d21fa7644d64b04f3e6ecb
Awilliams4321 <awilliams1234@live.com>
2020-07-31 10:54:57 -0600

    Defining a create action method that creates a new user instance with the params permitted in the private method 'user_params' and redirects to the Musicals index page if the new user is validated. If a users input is not valid, the page will be rerendered.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,22 @@ class UsersController < ApplicationController
     def new
     end
 
-    def show
+    def create
+        @new_user = User.new(user_params)
+
+        if @new_user
+            redirect_to musicals_path
+        else
+            render :new
+        end
     end
 
-    def create
-    end
+    def show
+    end 
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end 
 end
