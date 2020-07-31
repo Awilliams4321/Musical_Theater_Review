5c8f4f47e5650501fd14e51d9130c6dfe8bcf8d4
Awilliams4321 <awilliams1234@live.com>
2020-07-31 10:58:08 -0600

    Defining new and create methods that allow a new Review to be created and saved. Once saved, the User will be redirected to the Reviews index page. If fields are invalid, they will be redirected back to the new review form.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,9 +10,16 @@ class ReviewsController < ApplicationController
 
     def create
         @review = Review.new(review_params)
+        if @review.save
+            redirect_to reviews_path
+        else
+            redirect_to reviews_new_path
+        end 
     end 
 
     private
+    #Whitelisting attributes(permitting mass assignment)
+    #Strong params
 
     def review_params
         params.require(:review).permit(:headline, :content, :public_name)
