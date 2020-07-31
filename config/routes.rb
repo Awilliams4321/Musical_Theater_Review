d2d4762f79856c35da637fd559cb9717a4ae5b68
Awilliams4321 <awilliams1234@live.com>
2020-07-31 08:48:04 -0600

    Adding two github routes that will allow my app to use Github as an authentication provider and allow my Users to use Github as a sign in option.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+
+  root 'users#new'
+  resources :users, only: [:new, :show, :create]
+
   get '/reviews', to: 'reviews#index'
   get '/reviews/new', to: 'reviews#new'
   post '/reviews', to: 'reviews#create'
   get 'reviews/:id', to: 'reviews#show'
   get '/reviews/:id/edit', to: 'reviews#edit'
   resources :musicals
-  resources :users, only: [:new, :show, :create]
+
+
+  get '/auth/github', as: 'github'
+  get '/auth/github/callback', to: 'sessions#github'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
