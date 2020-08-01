1795dbc0eb5f739efa672749d03d546c68feec5e
Awilliams4321 <awilliams1234@live.com>
2020-07-31 23:11:35 -0600

    Connecting to my Ticketmaster API.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+co_venues = HTTParty.get("https://app.ticketmaster.com/discovery/v2/venues.json?apikey=AASuppw2AusY2e7kVgGpVxZQGT0LvQUu&locale=en-us&stateCode=CO&countryCode=US") 
 
-Review.create([{headline: "Great acting!"}, {headline: "Phenomenal show!"}, {headline: "Not impressed. Lacking depth."}])
+co_venues_ids = []
+
+vens = []
+co_venues["_embedded"]["venues"].each{|h| vens << {name: h["name"], api_id: h["id"], browser_url: h["url"]} }
+
+#v_hash represents the #=> 
+# {:name=>"Avalon Theatre",
+#   :api_id=>"KovZpa4gke",
+#   :bro_url=>"https://www.ticketmaster.com/avalon-theatre-tickets-grand-junction/venue/246458"}
+
+#hash, hits the API, 
+api_data = {}
+
+vens.each do |v_hash| #v_hash represents the hash, taking the api key value which rep. the venue in ticket master api, 
+   response = HTTParty.get("https://app.ticketmaster.com/discovery/v2/events.json?apikey=AASuppw2AusY2e7kVgGpVxZQGT0LvQUu&venueId=#{v_hash[:api_id]}")
+   ven_events = response["_embedded"]["events"].map{|hash| {event_name: hash["name"], browser_url: hash["url"], 
+   description: hash["info"], api_class: hash["classifications"], venue: v_hash[:name]}}
+   api_data[v_hash[:name]] = ven_events #Ex: 'Coors Fields': [{event_info}, {event_info}, etc.}]
+end
+
+# arts_n_threater = []
+arts_n_theatre = api_data.values.delete_if do |arr_h| 
+   arr_h[0][:api_class].nil? 
+end.select do |arr_h|
+    arr_h if arr_h[0][:api_class].first["segment"]["name"] == 'Arts & Theatre'
+end.flatten
+
+ binding.pry
+
+# https://app.ticketmaster.com/discovery/v2/events.json?apikey=AASuppw2AusY2e7kVgGpVxZQGT0LvQUu&venueId=KovZpa4gke
+
+# name, url, info, classifications
+
