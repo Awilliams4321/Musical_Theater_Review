c0cb5be77a40463259df8785b99dd6a0e794cf78
Awilliams4321 <awilliams1234@live.com>
2020-08-02 14:12:54 -0600

    Adding helper methods that assign a user a session key if they are logged in.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def is_logged_in?
+        session.has_key?(:user_id)
+    end 
+
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if is_logged_in?
+    end
+    
 end
