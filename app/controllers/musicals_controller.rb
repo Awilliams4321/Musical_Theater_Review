ab5c439ed87eb662617340df919327c18e90bc79
Awilliams4321 <awilliams1234@live.com>
2020-08-02 14:05:46 -0600

    Deleting my 'new' method and my private methods because they are unnecessary since a user will not be creating a new musical in the app.

--- app/controllers/musicals_controller.rb
+++ app/controllers/musicals_controller.rb
@@ -1,2 +1,11 @@
 class MusicalsController < ApplicationController
+
+    def index
+        @musicals = Musical.all
+    end
+
+    def show
+        @musical = Musical.find_by(params[:id])
+    end
+
 end
