79c1ec812d86b8f61ce0ce75ba02692be60a0c84
Awilliams4321 <awilliams1234@live.com>
2020-08-02 14:02:48 -0600

    Adding gems 'pry' and 'httparty'.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,34 @@
-co_venues = HTTParty.get("https://app.ticketmaster.com/discovery/v2/venues.json?apikey=AASuppw2AusY2e7kVgGpVxZQGT0LvQUu&locale=en-us&stateCode=CO&countryCode=US") 
+# co_venues = HTTParty.get("https://app.ticketmaster.com/discovery/v2/venues.json?apikey=AASuppw2AusY2e7kVgGpVxZQGT0LvQUu&locale=en-us&stateCode=CO&countryCode=US") 
 
-co_venues_ids = []
+# co_venues_ids = []
 
-vens = []
-co_venues["_embedded"]["venues"].each{|h| vens << {name: h["name"], api_id: h["id"], browser_url: h["url"]} }
+# vens = []
+# co_venues["_embedded"]["venues"].each{|h| vens << {name: h["name"], api_id: h["id"], browser_url: h["url"]} }
 
-#v_hash represents the #=> 
-# {:name=>"Avalon Theatre",
-#   :api_id=>"KovZpa4gke",
-#   :bro_url=>"https://www.ticketmaster.com/avalon-theatre-tickets-grand-junction/venue/246458"}
+# #v_hash represents the #=> 
+# # {:name=>"Avalon Theatre",
+# #   :api_id=>"KovZpa4gke",
+# #   :bro_url=>"https://www.ticketmaster.com/avalon-theatre-tickets-grand-junction/venue/246458"}
 
-#hash, hits the API, 
-api_data = {}
+# #hash, hits the API, 
+# api_data = {}
 
-vens.each do |v_hash| #v_hash represents the hash, taking the api key value which rep. the venue in ticket master api, 
-   response = HTTParty.get("https://app.ticketmaster.com/discovery/v2/events.json?apikey=AASuppw2AusY2e7kVgGpVxZQGT0LvQUu&venueId=#{v_hash[:api_id]}")
-   ven_events = response["_embedded"]["events"].map{|hash| {event_name: hash["name"], browser_url: hash["url"], 
-   description: hash["info"], api_class: hash["classifications"], venue: v_hash[:name]}}
-   api_data[v_hash[:name]] = ven_events #Ex: 'Coors Fields': [{event_info}, {event_info}, etc.}]
-end
+# vens.each do |v_hash| #v_hash represents the hash, taking the api key value which rep. the venue in ticket master api, 
+#    response = HTTParty.get("https://app.ticketmaster.com/discovery/v2/events.json?apikey=AASuppw2AusY2e7kVgGpVxZQGT0LvQUu&venueId=#{v_hash[:api_id]}")
+#    ven_events = response["_embedded"]["events"].map{|hash| {event_name: hash["name"], browser_url: hash["url"], 
+#    description: hash["info"], api_class: hash["classifications"], venue: v_hash[:name]}}
+#    api_data[v_hash[:name]] = ven_events #Ex: 'Coors Fields': [{event_info}, {event_info}, etc.}]
+# end
 
-# arts_n_threater = []
-arts_n_theatre = api_data.values.delete_if do |arr_h| 
-   arr_h[0][:api_class].nil? 
-end.select do |arr_h|
-    arr_h if arr_h[0][:api_class].first["segment"]["name"] == 'Arts & Theatre'
-end.flatten
+# arts_n_theatre = api_data.values.delete_if do |arr_h| 
+#    arr_h[0][:api_class].nil? 
+# end.select do |arr_h|
+#     arr_h if arr_h[0][:api_class].first["segment"]["name"] == 'Arts & Theatre'
+# end.flatten
 
- binding.pry
+#  binding.pry
 
-# https://app.ticketmaster.com/discovery/v2/events.json?apikey=AASuppw2AusY2e7kVgGpVxZQGT0LvQUu&venueId=KovZpa4gke
+# # https://app.ticketmaster.com/discovery/v2/events.json?apikey=AASuppw2AusY2e7kVgGpVxZQGT0LvQUu&venueId=KovZpa4gke
 
-# name, url, info, classifications
+# # name, url, info, classifications
 
