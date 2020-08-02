1cd0b2f052fba0d9af6e89dcaa0f7d437a800575
Awilliams4321 <awilliams1234@live.com>
2020-08-02 14:04:41 -0600

    Updating my Musicals table to include 'genre' and 'info' columns.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_151932) do
+ActiveRecord::Schema.define(version: 2020_08_02_163437) do
 
   create_table "musicals", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_07_30_151932) do
     t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "info"
+    t.string "genre"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -41,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_07_30_151932) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uid"
   end
 
 end
