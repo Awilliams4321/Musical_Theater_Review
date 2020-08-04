eca89ca8a1199c063bb7a3f9b44382a9dd748370
Awilliams4321 <awilliams1234@live.com>
2020-08-03 21:19:48 -0600

    Adding an if/else statement to my reviews index route to display my nested routes index if the page is navigated via the :musical_id params(/musicals/1/reviews) vs. all reviews(/reviews).

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,20 +1,33 @@
 class ReviewsController < ApplicationController
+    # before_action :set_method, only: [:new, :create, :update, :destroy] 
 
     def index
-        @reviews = Review.all
+        if params[:musical_id]
+            @musical = Musical.find(params[:musical_id])
+            @reviews = @musical.reviews
+        else
+            @reviews = Review.all
+        end
     end
+
+    def show 
+        @review = Review.find_by(params[:id])
+        @musical = Musical.find_by(params[:id])
+    end 
 
     def new
         @review = Review.new
         @musicals = Musical.all
+
+        @review.build_musical(params[:musical_id])
     end
 
     def create
-        @review = Review.new(review_params)
+        @review = Review.find_by(params[:id])
         if @review.save
-            redirect_to reviews_path
+            redirect_to review_path
         else
-            redirect_to reviews_new_path
+            render :new 
         end 
     end 
 
@@ -23,6 +36,6 @@ class ReviewsController < ApplicationController
     #Strong params
 
     def review_params
-        params.require(:review).permit(:headline, :content, :public_name)
+        params.require(:review).permit(:musical_id, :id, :headline, :content, :public_name)
     end 
 end
