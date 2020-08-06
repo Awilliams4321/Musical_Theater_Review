ae8962c9b68a9fe885bee5e6571a2623e39f14a4
Awilliams4321 <awilliams1234@live.com>
2020-08-05 21:10:59 -0600

    Adding the methods that were in my application controller to my application helper instead so that my views and controllers can access them.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,15 @@
 module ApplicationHelper
 
     def is_logged_in?
-        session.has_key?(:user_id)
-    end 
-
+        unless session.has_key?(:user_id)
+          flash[:error] = "You must be logged in to access this section"
+          redirect_to root_path 
+        end
+      end
+    
     def current_user
-      @current_user ||= User.find(session[:user_id]) if is_logged_in?
-    end
+        @current_user ||= User.find(session[:user_id]) if is_logged_in?
+      end
+    
     
 end
