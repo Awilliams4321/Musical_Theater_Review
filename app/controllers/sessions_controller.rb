5c5dd3294db2263c15bb091de0516ba9a53cde03
Awilliams4321 <awilliams1234@live.com>
2020-08-12 09:02:30 -0600

    Making Omniauth alterations to my github methods.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,18 @@
 class SessionsController < ApplicationController
 
     def new
+        render :login
     end
 
-    def create
-        user = User.find_by(email: session_params[:email])
-        
-        if user && user.authenticate(session_params[:password])
-            session[:user_id] = user.id
-            redirect_to musicals_path
+    def login
+        @user = User.find_by(email: params[:email])
+       
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to users_show_path(@user)
+            binding.pry
         else
+            # flash[:error] = "Invalid Email or Password. Please Try Again."
             redirect_to login_path
         end
     end
@@ -20,7 +23,7 @@ class SessionsController < ApplicationController
     end
 
     def github
-        
+       
         user = User.find_or_create_by(uid: github_params['uid']) do |u|
             u.username = github_params['info']['name'] || github_params['info']['nickname']
             u.email = github_params['info']['email']
@@ -34,7 +37,7 @@ class SessionsController < ApplicationController
     end
 
     private
-    
+   
     def session_params
         params.require(:session).permit(:username, :email, :password)
     end
@@ -42,6 +45,5 @@ class SessionsController < ApplicationController
     def github_params
         request.env['omniauth.auth']
     end
-
 
 end
