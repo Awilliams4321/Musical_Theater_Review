5324fbc6eb21717a07f702a1abc8b2d1b0715880
Awilliams4321 <awilliams1234@live.com>
2020-08-12 09:04:02 -0600

    Just Changing the spacing of my methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,27 @@
 class UsersController < ApplicationController
-
     def new
+        @user = User.new
     end
 
     def create
-        @new_user = User.new(user_params)
+       
+        @user = User.new(user_params)
 
-        if @new_user
-            redirect_to musicals_path
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
         else
-            render :new
+            flash[:error] = "Invalid Email or Password. Please Try Again."
+            redirect_to '/login'
         end
     end
 
     def show
+        @user = User.find(params[:id])
     end 
 
     private
-    
+   
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end 
