60742014bb64d94280faaf0854bced4d2fbb6772
Awilliams4321 <awilliams1234@live.com>
2020-08-12 08:59:10 -0600

    Adding Omniauth methods to my Reviews Controller.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,67 +1,126 @@
 class ReviewsController < ApplicationController
-    # before_action :set_method, only: [:new, :create, :update, :destroy] 
-    before_action :is_logged_in?, except: :index
-
-    def index
-        if params[:musical_id]
-            @musical = Musical.find(params[:musical_id])
-            @reviews = @musical.reviews
-        else
-            @reviews = Review.all
-        end
-    end
-
-    def show 
-        @review = Review.find_by(params[:id])
-        @musical = Musical.find_by(params[:id])
-    end 
+    # before_action :set_method, only: [:new, :create, :update, :destroy]
+    # before_action :is_logged_in?
 
     def new
         @review = Review.new
         @musicals = Musical.all
+        @musical = Musical.find_by(id: params[:musical_id])
+        # @musical = Musical.find_by(params[:id])
 
-        @review.build_musical(params[:musical_id])
+        @review.build_musical(id: params[:musical_id])
     end
 
     def create
-        @musical = Musical.find_by(params[:id])
-        @review = Review.new(review_params)
+        @musical = Musical.find(params[:musical_id])
+        @review = @musical.reviews.create(review_params)
         if @review.save
-            redirect_to review_path(@review)
+            redirect_to musical_reviews_path(@review)
         else
             render :new 
         end 
     end 
 
-    def edit
-        # redirect to "/" if !is_logged_in?
+    def index
+        if params[:musical_id]
+            @musicals = Musical.all
+            @reviews = Review.all
+        else
+            @review = Review.find_by(id: params[:id])
+        end
+    end 
 
-        @musical = Musical.find_by(params[:id])
-        @review = Review.find_by(params[:id])
+    def show 
+        @review = Review.find_by(id: params[:id])
+
+    end 
+
+    def edit
+        @musical = Musical.find_by(id: params[:musical_id])
+        @review = @musical.reviews 
+        # Review.find_by(params[:id])
     end
 
     def update
-        # redirect to "/" if !is_logged_in?
-
         @review = Review.find(params[:id])
         @review.update(review_params)
-        redirect_to review_path(@review)
+        redirect_to musical_review_path(@review)
     end
 
     def destroy
-        # redirect to "/" if !is_logged_in?
-
-        @review = Review.find_by(params[:id])
+        @review = Review.find_by(id: params[:id])
         @review.destroy
         redirect_to reviews_path
     end
-        
+       
 
     private
     #Whitelisting attributes(permitting mass assignment)
     #Strong params
 
     def review_params
-        params.require(:review).permit(:musical_id, :headline, :content, :public_name)
+        params.require(:review).permit(:musical_id, :headline, :content, :public_name, musicals_attributes: [:name, :musical_id]) #users and musicals attr.
     end 
+
+    def is_logged_in?
+        unless session.has_key?(:user_id)
+            flash[:error] = "You must be logged in to access this section"
+            redirect_to root_path
+        end
+    end
+   
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if is_logged_in?
+    end
+end
+
+
+class SessionsController < ApplicationController
+
+    def new
+        render :login
+    end
+
+    def login
+        @user = User.find_by(email: params[:email])
+       
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to users_show_path(@user)
+            binding.pry
+        else
+            # flash[:error] = "Invalid Email or Password. Please Try Again."
+            redirect_to login_path
+        end
+    end
+
+    def logout
+        session.clear
+        redirect_to login_path
+    end
+
+    def github
+       
+        user = User.find_or_create_by(uid: github_params['uid']) do |u|
+            u.username = github_params['info']['name'] || github_params['info']['nickname']
+            u.email = github_params['info']['email']
+            u.password = github_params['credentials']['token']
+          end
+       
+          session[:user_id] = user.id
+       
+          redirect_to musicals_path
+
+    end
+
+    private
+   
+    def session_params
+        params.require(:session).permit(:username, :email, :password)
+    end
+
+    def github_params
+        request.env['omniauth.auth']
+    end
+
 end
