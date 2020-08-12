7fc8e9f4b0245851854020ebc51eaabab1e58189
Awilliams4321 <awilliams1234@live.com>
2020-08-12 08:57:50 -0600

    Adding strong params to my Musical controller.

--- app/controllers/musicals_controller.rb
+++ app/controllers/musicals_controller.rb
@@ -1,11 +1,18 @@
 class MusicalsController < ApplicationController
 
     def index
+        @user = User.find_by(id: params[:id])
         @musicals = Musical.all
     end
 
     def show
         @musical = Musical.find_by(id: params[:id])
     end
+
+    private
+
+    def musical_params
+        params.require(:musical).permit(:name, :city, :state, :venue, :date, :info, :genre) #add reviews_attr.
+    end 
 
 end
