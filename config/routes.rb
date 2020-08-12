7de57a39bb2bac700793f3ff90b48b045437fee0
Awilliams4321 <awilliams1234@live.com>
2020-08-12 09:07:33 -0600

    Nesting my reviews routes inside of my musicals routes via a resources do block.

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,29 @@
 Rails.application.routes.draw do
 
-  root 'sessions#new'
+  root 'users#new'
   resources :users, only: [:new, :show, :create]
 
   get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
+  post '/login', to: 'sessions#login'
   delete '/logout', to: 'sessions#logout'
 
   resources :musicals, only: [:index] do
-    resources :reviews, only: [:show, :index, :update]
+    resources :reviews
   end
 
   get '/musicals/:id', to: 'musicals#show', as: 'musical'
 
   get '/reviews', to: 'reviews#index'
-  get '/reviews/new', to: 'reviews#new'
+  # get '/reviews/new', to: 'reviews#new'
   post '/reviews', to: 'reviews#create'
-  get '/reviews/:id', to: 'reviews#show', as: 'review'
+  # get '/reviews/:id', to: 'reviews#show', as: 'review'
   get '/reviews/:id/edit', to: 'reviews#edit'
   patch '/reviews/:id', to: 'reviews#update'
-  delete '/reviews/:id', to: 'reviews#destroy'
+  # delete '/reviews/:id', to: 'reviews#destroy'
 
 
   get '/auth/github', as: 'github'
+  get '/auth/github/callback', to: 'sessions#github'
   get '/auth/github/callback', to: 'sessions#github'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
