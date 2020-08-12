288e9d878f498ca3bb7f0a8e043adbfa202372de
Awilliams4321 <awilliams1234@live.com>
2020-08-12 09:06:08 -0600

    Adding a scope method that orders reviews from most recent to oldest.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,18 @@
 class Review < ApplicationRecord
     validates :headline, presence: true
-    belongs_to :user
+    belongs_to :user, optional: true
     belongs_to :musical
+
+    accepts_nested_attributes_for :musical
+
+    scope :most_recent, -> { order(created_at: :desc) }
+
+    def review_id=(id)
+        review = Review.find_or_create_by(id: id)
+        self.id = id
+    end
+
+    def review_name 
+        self.id ? self.id : nil 
+    end
 end
