7039e19239f50cb21a22288f29164866a10daa5a
Awilliams4321 <awilliams1234@live.com>
2020-08-12 09:05:18 -0600

    Adding a method that allows me to accept nested attributes for reviews.

--- app/models/musical.rb
+++ app/models/musical.rb
@@ -1,9 +1,18 @@
 class Musical < ApplicationRecord
-
     has_many :reviews
     has_many :users, through: :reviews
 
-    accepts_nested_attributes_for :reviews
+    accepts_nested_attributes_for :reviews, allow_destroy: true
+
+
+    def musical_id=(id)
+        musical = Musical.find_or_create_by(id: id)
+        self.id = id
+    end
+
+    def musical_name 
+        self.id ? self.id : nil 
+    end
 
 end
  
