71199681bee40cd468acd8640e802da8c0928d7a
Awilliams4321 <awilliams1234@live.com>
2020-08-13 11:38:14 -0600

    Adding a require_login method and a current user method to access in my Reviews Controller.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,17 @@
 module ApplicationHelper
 
-    def is_logged_in?
-        unless session.has_key?(:user_id)
-          flash[:error] = "You must be logged in to access this section"
-          redirect_to root_path 
-        end
-      end
-    
-    def current_user
-        @current_user ||= User.find(session[:user_id]) if is_logged_in?
-      end
-    
-    
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def require_login
+    if !session.include? :user_id
+      flash[:errors] = "You must be logged in to do that!"
+      redirect_to root_path
+    end 
+  end
+
 end
+  
+    
+    
