ae0344fda4c2771d09ec6b8ae42f20ad10a48062
Awilliams4321 <awilliams1234@live.com>
2020-08-19 13:58:42 -0600

    Validating the uniqueness of a users email upon sign in.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     
     validates :email, presence: true, unless: :uid
-    validates :username, uniqueness: true
+    validates :email, uniqueness: true
 
     has_many :reviews
     has_many :musicals, through: :reviews
